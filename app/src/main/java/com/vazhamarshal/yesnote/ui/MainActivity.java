2f1eb17be0f747776041c10f03f413cc5ed7fc50
WilliamMarshal <vazha.bichiashvili04@geolab.edu.ge>
2016-12-07 01:04:56 +0400

    add note activity added

--- app/src/main/java/com/vazhamarshal/yesnote/ui/MainActivity.java
+++ app/src/main/java/com/vazhamarshal/yesnote/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.vazhamarshal.yesnote.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.vazhamarshal.yesnote.R;
 import com.vazhamarshal.yesnote.adapter.NoteAdapter;
@@ -22,6 +25,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.add_note_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newNoteIntent = new Intent(MainActivity.this, NewNoteActivity.class);
+                startActivity(newNoteIntent);
+            }
+        });
 
         mNoteRecyclerView = (RecyclerView) findViewById(R.id.note_recycler_view);
         mNoteRecyclerView.setLayoutManager(new LinearLayoutManager(this));
